31d483b39cf0b588dc3ebe830891ffaa502ef183
calculator <efremov.200328@gmail.com>
2024-09-29 16:24:23 +0700

    Edit gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pmu1"
+    namespace = "com.example.pmu1"  // Убедитесь, что это правильное пространство имен для вашего проекта
     compileSdk = 34
 
     defaultConfig {
@@ -53,13 +53,18 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui") // Версия из BOM
-    implementation("androidx.compose.ui:ui-graphics") // Версия из BOM
-    implementation("androidx.compose.material3:material3") // Версия из BOM
-    implementation("androidx.compose.ui:ui-tooling-preview") // Версия из BOM
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0-alpha01")
+    implementation("androidx.compose.foundation:foundation:1.5.0")
+    implementation("androidx.compose.ui:ui:1.5.0")
+    implementation("androidx.compose.material3:material3:1.0.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    
+    implementation ("androidx.activity:activity-ktx:1.7.2")
     implementation("androidx.compose.runtime:runtime-livedata:1.0.0-beta01")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
